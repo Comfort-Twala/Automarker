2bc681c38456fa0228fa6e931ec8081c6c5aaf38
Comfort-Twala <kontreitroos@gmail.com>
2021-10-13 03:35:48 +0200

    Output Tests Added

---
+++ src/test/OutputTest.java
@@ -0,0 +1,68 @@
+package test;
+
+// import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+import main.Output;
+
+public class OutputTest extends TestCase{
+	
+	private Output output;
+	private	String[] cars;
+	private ArrayList<String> carsList;
+	/**
+	 * Setup method	
+	 */
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		this.output = new Output();
+		cars = new String[]{"Volvo", "BMW", "Ford", "Mazda", "Mercedes"};
+		carsList = new ArrayList<>(Arrays.asList(cars));
+	}
+
+	/**
+	 * Testing addInput method
+	 */
+	@Test
+	public void testAddInput(){
+		int size = 0;
+		for (String car : cars) {
+			output.addInput(car);
+			size++;
+			assertEquals(size, output.size());
+		}
+	}
+
+	/**
+	 * Testing getOutput method
+	 */
+	@Test
+	public void testGetOutput() {
+		for (String car : cars) {
+			output.addInput(car);
+		}
+		assertEquals(carsList.toString(), output.getOutput().toString());
+	}
+
+	/**
+	 * Testing compareTo method
+	 */
+	@Test
+	public void testCompareTo() {
+		Output other = new Output();
+		for (String car : cars) {
+			output.addInput(car);
+			other.addInput(car);
+		}
+		assertEquals(0, output.compareTo(other));
+
+		other.addInput("Ferari");
+		assertEquals(-1, output.compareTo(other));
+	}
+}
